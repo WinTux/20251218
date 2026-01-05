f5d4c549a4fa3de030f4e5e1a119ae70a526dbff
rusok <rusok.software@gmail.com>
2026-01-05 19:56:01 -0400

    Kubernetes

---
+++ Campus/Controllers/HistorialController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Campus.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HistorialController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return "";
+        }
+    }
+}
